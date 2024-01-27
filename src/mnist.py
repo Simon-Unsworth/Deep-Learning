a8309ae3f0b3d7157322cab22f84ec88dc5b4d88
Simon-Unsworth <simonunsworth@outlook.ie>
2024-01-27 11:20:59 +0000

    Updated output

--- src/mnist.py
+++ src/mnist.py
@@ -19,7 +19,6 @@ def main() -> None:
 
     test_loss, test_acc = model.evaluate(x=x_test, y=y_test, verbose=0)
 
-    print("Metrics")
     print(f"Test Loss: {test_loss*100:.2f}%")
     print(f"Test Accuracy: {test_acc*100:.2f}%")
 
